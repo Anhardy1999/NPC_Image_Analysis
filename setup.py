32621b4f4af78d36de905b3d60d6f5fb27cc7cc8
Anhardy1999 <96258085+Anhardy1999@users.noreply.github.com>
2022-05-24 22:28:33 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
+    package_dir={"": "docs"},
+    packages=setuptools.find_packages(where="docs"),
     python_requires=">=3.6",
 )
