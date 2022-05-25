de964d5b55bf493e45bf8a45a0cfc50368df2922
Anhardy1999 <96258085+Anhardy1999@users.noreply.github.com>
2022-05-24 22:51:52 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -49,6 +49,11 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+autodoc_mock_imports = [
+    "napari",
+    "pandas"
+]
+
 
 # -- Options for HTML output -------------------------------------------------
 
